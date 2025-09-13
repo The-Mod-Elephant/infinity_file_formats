06684524f0948a3da44e69b26f6bcd6b4b67a8ce
dark0dave <dark0dave@mykolab.com>
2025-09-13 14:28:00 +0100

    feat(writer): GLORIOUS WRITER
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
-with import <nixpkgs> {};
-pkgs.mkShell {
-  buildInputs = [
-    delve
-    git
-    go
-    pre-commit
-  ];
-}
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+  pkgs = import <nixpkgs> {};
+  unstable = import unstableTarball {};
+  shell = pkgs.mkShell {
+    buildInputs = [
+      unstable.delve
+      pkgs.git
+      unstable.go
+      unstable.gopls
+      unstable.go-tools
+      pkgs.pre-commit
+    ];
+  };
+in shell
