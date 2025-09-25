22a20abe62b1c4daa9b9b9c8424f5dc0e0824065
dark0dave <dark0dave@mykolab.com>
2025-09-25 16:31:42 +0100

    feat(flake): Add flake
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- shell.nix
+++ shell.nix
@@ -1,17 +1,27 @@
 let
   unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-  pkgs = import <nixpkgs> {};
-  unstable = import unstableTarball {};
+    fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/2cd3cac16691a933e94276f0a810453f17775c28.tar.gz";
+      sha256 = "06ad2257srqw8q4504hqm5hyb50vlckhgdfcjx81hxq3l1wy9i5j";
+    };
+  pkgs = import unstableTarball {};
   shell = pkgs.mkShell {
+    # Libs
     buildInputs = [
-      unstable.delve
+      pkgs.go
+    ];
+    # Tools
+    nativeBuildInputs = [
+      pkgs.delve
       pkgs.git
-      unstable.go
-      unstable.gopls
-      unstable.go-tools
+      pkgs.gopls
+      pkgs.go-tools
       pkgs.pre-commit
     ];
+    shellHook = ''
+    # Make sure Go always has a valid temp dir
+    export TMPDIR=$(mktemp -d)
+    pre-commit install -f
+    '';
   };
 in shell
